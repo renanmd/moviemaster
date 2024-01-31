8407a48557882af10fba4b2e7d72f142e7b64fc3
Renan Muller <renanmd@gmail.com>
2024-01-31 13:45:42 -0300

    Initial Commit

---
+++ MovieMaster/MovieMasterApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieMasterApp.swift
+//  MovieMaster
+//
+//  Created by Renan Diniz on 31/01/24.
+//
+
+import SwiftUI
+
+@main
+struct MovieMasterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
