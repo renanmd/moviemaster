76642ed41cfa276e55b1969cf1043d19c61ecb1c
Renan Muller <renanmd@gmail.com>
2024-02-02 18:11:17 -0300

    WIP login screen.

--- MovieMaster/ContentView.swift
+++ MovieMaster/ContentView.swift
@@ -1,21 +1,88 @@
-//
-//  ContentView.swift
-//  MovieMaster
-//
-//  Created by Renan Diniz on 31/01/24.
-//
-
 import SwiftUI
 
 struct ContentView: View {
+    @State private var username: String = ""
+    @State private var password: String = ""
+    @State private var showAlert: Bool = false
+    @State private var loginBtnDisabled: Bool = true
+
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            BackgroundImageView()
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                HStack{
+                    Text("User:")
+                        .font(.subheadline)
+                        .foregroundColor(.white)
+                        .padding(.leading)
+                    TextField("Username", text: $username)
+                        .padding()
+                        .background(Color.white)
+                        .cornerRadius(10)
+                        .padding(.trailing)
+                        .autocapitalization(.none)
+                        .disableAutocorrection(true)
+                        .onChange(of: username) { newValue in
+                            let filtered = newValue.filter { $0.isLetter || $0.isNumber }
+                            if filtered != newValue {
+                                self.username = filtered
+                            }
+                        }
+                    
+                }
+                
+                HStack {
+                    Text("Password:")
+                        .font(.subheadline)
+                        .foregroundColor(.white)
+                        .padding(.leading)
+                    SecureField("Password", text: $password)
+                        .padding()
+                        .background(Color.white)
+                        .cornerRadius(10)
+                        .padding(.trailing)
+                }
+                .padding()
+                        .background(Color.clear)
+                        .padding(.leading)
+                        .alert(isPresented: $showAlert) {
+                            Alert(title: Text("Invalid Credentials"), message: Text("Please enter a valid username and password."), dismissButton: .default(Text("OK")))
+                        }
+                Button(action: {
+                                if isValidCredentials() {
+                                    // Perform login logic
+                                } else {
+                                    showAlert = true
+                                }
+                            }) {
+                                Text("Login")
+                                    .font(.subheadline)
+                                    .foregroundColor(.white)
+                                    .padding()
+                                    .background(Color.blue)
+                                    .cornerRadius(10)
+                            }
+            }
+            .padding()
         }
-        .padding()
+        .background(Color.clear)
+    }
+    
+    func isValidCredentials() -> Bool {
+        if username != "renanmd" || password != "123abc" {
+            return false
+        }
+            return !username.isEmpty && !password.isEmpty
+        }
+}
+
+struct BackgroundImageView: View {
+    var body: some View {
+        Image("MovieBackgroundDalle")
+            .resizable()
+            .aspectRatio(contentMode: .fill)
     }
 }
 
